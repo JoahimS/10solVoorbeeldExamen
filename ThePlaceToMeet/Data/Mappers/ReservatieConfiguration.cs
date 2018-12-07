2fe80e4f803f94113b38c9c69bd2cbc95f7bfe5c
Pieter Van Der Helst <pieter.vanderhelst@hogent.be>
2018-12-07 10:16:32 +0100

    EF, mapping

--- ThePlaceToMeet/Data/Mappers/ReservatieConfiguration.cs
+++ ThePlaceToMeet/Data/Mappers/ReservatieConfiguration.cs
@@ -8,7 +8,10 @@ namespace ThePlaceToMeet.Data.Mappers
     {
         public void Configure(EntityTypeBuilder<Reservatie> builder)
         {
-            //implemsenteer
+            //implementeer
+            builder.ToTable("Reservatie");
+            builder.HasOne(t => t.Catering).WithMany().IsRequired(false).OnDelete(DeleteBehavior.Restrict);
+            builder.HasOne(t => t.Korting).WithMany().IsRequired(false).OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
